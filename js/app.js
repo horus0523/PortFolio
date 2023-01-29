f9266c64e7c2661e39206957be9258c3e894e200
HorusCh <37905833+horus0523@users.noreply.github.com>
2023-01-29 18:14:55 -0300

    conexión de enlaces del menu
    -Se conectan enlaces del menu para el corecto funcionamiento de la pagina
    -Se mejora la funcionalidad

--- js/app.js
+++ js/app.js
@@ -8,3 +8,35 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+/* Permite que el menu hambuerguesa tenga funcionalidad y cierre el menu al seleccionar una opción */
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        const id = entry.target.getAttribute("id");
+        const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+
+        if (entry.isIntersecting) {
+            document.querySelector(".menu a.selected").classList.remove("selected");
+            menuLink.classList.add("selected");
+        } else {
+            menulink.classList.remove("selected");
+        }
+    });
+}, {rootMargin: "-30% 0px -70% 0px"});
+
+menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", function() {
+        menu.classList.remove("menu_opened");
+    });
+
+    /* observer */
+    const hash = menuLink.getAttribute("href");
+    const target = document.querySelector(hash);
+    if (target) {
+        observer.observe(target);
+    }
+    /* observer */
+});
